325136ae45be1227bd05dc24b4bd70aa367e6cb1
Administrator <761780862@qq.com>
2020-10-19 09:21:00 +0800

    deploy

---
+++ precache-manifest.508c920da64258e3e3c1716831f73579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0c8e96369a40d42cdf",
+    "url": "css/app.26fbb8b7.css"
+  },
+  {
+    "revision": "b5e2c5b423e14d1b8a365b87322090b6",
+    "url": "index.html"
+  },
+  {
+    "revision": "da0c8e96369a40d42cdf",
+    "url": "js/app.851458dd.js"
+  },
+  {
+    "revision": "d0aac6f1a6f34d03731b",
+    "url": "js/chunk-vendors.94e03280.js"
+  },
+  {
+    "revision": "0a5f5cd8b59501d25899e0dfdee479a4",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
